271fcd52ea62bae5e0816b09bdd350db0a8574ff
Helder <helder.devjava@gmail.com>
2023-02-22 01:13:48 -0300

    até que fim coloquei os icones

--- App.tsx
+++ App.tsx
@@ -8,9 +8,6 @@
 import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
@@ -24,6 +21,14 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from './src/screens/Home';
+import Chat from './src/screens/Chat';
+import Lista from './src/screens/Lista';
+import { ChatText, ListPlus, House } from 'phosphor-react-native';
+
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -55,6 +60,8 @@ function Section({children, title}: SectionProps): JSX.Element {
   );
 }
 
+const Tab = createBottomTabNavigator();
+
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
@@ -63,36 +70,18 @@ function App(): JSX.Element {
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name='Home' component={Home} options={{ tabBarIcon: () => 
+        (<House size={32} />)
+      }}/>
+      <Tab.Screen name='Lista' component={Lista} options={{tabBarIcon: () => 
+        (<ListPlus size={32} />)}}/>
+      <Tab.Screen name='Chat' component={Chat} options={{tabBarIcon: () =>(
+        <ChatText size={32} />
+      )}}/>
+    </Tab.Navigator>
+  </NavigationContainer>
   );
 }
 
---
+++ src/components/TabUser/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {Text } from "react-native"
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from "../../screens/Home";
+import Sobre from "../../screens/Sobre";
+
+const Tab = createBottomTabNavigator();
+const TabUser = ()=> {
+    return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name='Home' component={Home}/>
+        <Tab.Screen name='Sobre' component={Sobre}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+    );
+}
+  
+export default TabUser;
+
